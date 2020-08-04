d0c0919b00b0ea0a6b34f83e55b4794d303d2610
Nadiya <nadiya.abdi@zoopla.co.uk>
2020-08-04 11:59:14 +0100

    Add 'Submit' button

--- src/Settings/Settings.js
+++ src/Settings/Settings.js
@@ -1,11 +1,16 @@
 ﻿import React from "react";
+import { Preview } from '../Preview/Preview';
 
 export const Settings = () => {
     return (
         <section>
             <h2>Settings</h2>
             <div>
-                TODO: Add the settings.
+                <form action="/action_page.php">
+                    <label for="username">Input Title: </label>
+                    <input type="text" id="username" name="username"></input>
+                    <input type="submit" value="Submit"></input>
+                </form>
             </div>
         </section>
     );
